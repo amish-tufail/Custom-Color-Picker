0ee403d114fb937ef74cfc119356041367f1ce7f
Amish <amish.tufail2002@gmail.com>
2024-02-03 17:29:01 +0500

    - Created UI. - Added Selection functionality - Complete

--- CustomColorPicker/ContentView.swift
+++ CustomColorPicker/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colors: [Color] = [
+        .cyan, .yellow, .green, .blue, .red, .orange, .pink, .indigo, .teal, .mint
+    ]
+    @State private var selectedColor: Color = .cyan
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            HStack {
+                RoundedRectangle(cornerRadius: 20.0, style: .continuous)
+                    .fill(selectedColor.gradient)
+                    .frame(width: 100.0, height: 100.0)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 20.0, style: .continuous)
+                            .stroke(.black.opacity(0.1), lineWidth: 2.0)
+                    }
+                    .shadow(color: .black.opacity(0.1), radius: 5.0)
+                LazyHGrid(rows: [GridItem(), GridItem()], content: {
+                    ForEach(colors, id: \.self) { color in
+                        Circle()
+                            .foregroundStyle(color.gradient)
+                            .opacity(0.8)
+                            .frame(width: 40.0, height: 40.0)
+                            .scaleEffect(selectedColor == color ? 0.7 : 1.0)
+                            .onTapGesture {
+                                withAnimation(.snappy) {
+                                    selectedColor = color
+                                }
+                            }
+                            .overlay {
+                                if selectedColor == color {
+                                    Circle()
+                                        .stroke(.black, lineWidth: 3.0)
+                                }
+                            }
+                    }
+                })
+            }
+            .frame(height: 100.0)
+            .padding()
+            .background(selectedColor.opacity(0.1).gradient)
+            .clipShape(.rect(cornerRadius: 20.0, style: .continuous))
+            .navigationTitle("Color Picker 🎨")
         }
-        .padding()
     }
 }
 
