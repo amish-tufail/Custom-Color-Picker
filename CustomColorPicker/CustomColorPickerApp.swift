f2657a81f4bf846d2c7dbbb74685a3b5108aa48e
Amish <amish.tufail2002@gmail.com>
2024-02-03 17:11:23 +0500

    Initial Commit

---
+++ CustomColorPicker/CustomColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColorPickerApp.swift
+//  CustomColorPicker
+//
+//  Created by Amish Tufail on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
